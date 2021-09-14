3136b06e20be1109bd660e0aa8ce4349cad456cd
Waleed Ali Khan <waleedomore@gmail.com>
2021-09-15 00:48:20 +0500

    updates for test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,9 @@
 import { FunctionalComponent, h } from 'preact';
-import { Route, Router } from 'preact-router';
-
-import Home from '../routes/home';
-import Profile from '../routes/profile';
-import NotFoundPage from '../routes/notfound';
-import Header from './header';
 
 const App: FunctionalComponent = () => {
     return (
         <div id="preact_root">
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
+            <h1>HEllo World!</h1>
         </div>
     );
 };
